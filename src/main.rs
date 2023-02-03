fff379526df443455272a6bbca22fe622e07b162
kilometers <kilo@meters.sh>
2023-02-02 16:09:03 -0800

    Make rolling more fun

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use colored::Colorize;
-use comfy_table::{presets::UTF8_FULL, Table};
-use indicatif::ProgressBar;
-use rand::{seq::IteratorRandom, thread_rng};
+use comfy_table::{presets::UTF8_FULL, CellAlignment, ColumnConstraint, Row, Table, Width::Fixed};
+use crossterm::{
+    cursor::{Hide, MoveDown, MoveUp, Show},
+    execute,
+    style::Stylize,
+    terminal::{Clear, ClearType},
+};
+use rand::{
+    seq::{IteratorRandom, SliceRandom},
+    thread_rng, Rng,
+};
 use std::{
     collections::BTreeSet,
     env,
     fs::{self, File, OpenOptions},
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{stdout, BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
     process::Command,
     thread,
@@ -51,20 +58,24 @@ fn main() -> Result<()> {
 
     match arguments.command {
         Commands::Add { name } => {
-            println!("Added {} to {}", name.underline(), list_name.bold().green());
+            println!(
+                "Added {} to {}",
+                name.as_str().underlined(),
+                list_name.bold().green()
+            );
             list.add(name);
         }
         Commands::Remove { name } => {
             println!(
                 "Removing {} from {}",
-                name.underline(),
+                name.as_str().underlined(),
                 list_name.bold().green()
             );
             if !list.remove(&name) {
                 println!(
                     "{} {} was not in list",
                     "error:".bold().red(),
-                    name.underline()
+                    name.underlined()
                 );
             }
         }
@@ -74,6 +85,7 @@ fn main() -> Result<()> {
             for (i, item) in list.items.iter().enumerate() {
                 table.add_row(vec![(i + 1).to_string(), item.to_string()]);
             }
+
             println!("{table}");
         }
         Commands::Roll => {
@@ -83,27 +95,67 @@ fn main() -> Result<()> {
                 list.items.len().to_string().bold()
             );
 
-            // make it dramatic
-            let bar = ProgressBar::new(list.items.len() as u64);
+            let mut rng = thread_rng();
 
-            for _ in 0..list.items.len() {
+            let mut choices = list.items.iter().collect::<Vec<_>>();
+            choices.shuffle(&mut rng);
+
+            let count = rng.gen_range(300..500);
+
+            let mut first = true;
+
+            execute!(stdout(), Hide)?;
+
+            for _ in 0..count {
+                for i in 0..(choices.len() - 1) {
+                    if rng.gen_bool(0.2) {
+                        choices.swap(i, i + 1);
+                    }
+                }
+
+                for i in 1..choices.len() {
+                    if rng.gen_bool(0.2) {
+                        choices.swap(i, i - 1);
+                    }
+                }
+
+                let mut table = Table::new();
+                table.load_preset(UTF8_FULL).set_header(vec!["#", "Title"]);
+
+                let column = table.column_mut(1).unwrap();
+                column.set_constraint(ColumnConstraint::Absolute(Fixed(36)));
+                column.set_cell_alignment(CellAlignment::Left);
+
+                for (i, item) in choices.iter().take(10).enumerate() {
+                    let mut row = Row::from(vec![(i + 1).to_string(), item.to_string()]);
+                    row.max_height(1);
+
+                    table.add_row(row);
+                }
+
+                if !first {
+                    execute!(stdout(), MoveUp(23))?;
+                }
+
+                for i in 0..choices.len() {
+                    // kill a random element
+                    // the `i < choices.len()` is not redundant
+                    if rng.gen_bool(0.001) && i < choices.len() {
+                        execute!(stdout(), Clear(ClearType::CurrentLine))?;
+                        println!("dead: {}", choices.remove(i).as_str().dark_red().bold());
+                    }
+                }
+
+                println!("{table}");
+
                 thread::sleep(Duration::from_millis(200));
-                bar.inc(1);
+
+                first = false;
             }
 
-            bar.finish_and_clear();
-
-            let choice = list.take_random();
-            let message = match choice {
-                Some(choice) => format!("  rolled: {}", choice.bold().green().reversed()),
-                None => format!(
-                    "{} No marbles. You can add some with\n    {}",
-                    "error:".bold().red(),
-                    "marbles add <NAME>".bold()
-                ),
-            };
-
-            println!("{message}");
+            let choice = choices[0];
+            println!("  rolled: {}", choice.as_str().bold().green().reverse());
+            execute!(stdout(), Show)?;
         }
         Commands::Edit => {
             let Ok(mut child) = Command::new(env::var("EDITOR").unwrap_or_else(|_| "vim".to_string())).arg(&list.path).spawn() else {
@@ -163,13 +215,6 @@ impl ItemList {
         writer.flush()?;
 
         Ok(())
-    }
-
-    fn take_random(&mut self) -> Option<String> {
-        let mut rng = thread_rng();
-        let item = self.items.iter().choose(&mut rng)?.to_string();
-        self.items.remove(&item);
-        Some(item)
     }
 
     fn add(&mut self, item: String) {
